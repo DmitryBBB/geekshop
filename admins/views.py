d8837f67973068944efef606feb513e9db965827
DmitryBBB <ark-bykov@yandex.ru>
2021-12-10 17:55:58 +0300

    add create_users

--- admins/views.py
+++ admins/views.py
@@ -1,21 +1,38 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from admins.forms import UserAdminRegisterForm
 from authapp.models import User
+
+
 # Create your views here.
 
 
 def index(request):
-    return render(request,'admins/admin.html')
+    return render(request, 'admins/admin.html')
 
 
 def admin_users(request):
     context = {
         "users": User.objects.all()
     }
-    return render(request,'admins/admin-users-read.html',context)
+    return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-read-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {
+        'title': 'GeekShop - Admin | Registration',
+        'form': form
+    }
+    return render(request, 'admins/admin-users-create.html',context)
 
 
 def admin_users_update(request):
@@ -23,4 +40,4 @@ def admin_users_update(request):
 
 
 def admin_users_delete(request):
-    return render(request, 'admins/admin-users-update-delete')
+    return render(request, 'admins/admin-users-update-delete.html')
