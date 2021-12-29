e3ef5ac0f194b1207e9b97dd3a9afa4fbca7f629
DmitryBBB <ark-bykov@yandex.ru>
2021-12-29 22:31:51 +0300

    add model UserProfile_vk

--- authapp/models.py
+++ authapp/models.py
@@ -1,11 +1,13 @@
-from datetime import timedelta
+
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
-from django.forms import forms
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from datetime import timedelta
 
 from django.utils.timezone import now
@@ -26,7 +28,26 @@ class User(AbstractUser):
 
 
 
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
 
+    GENDER_CHOICES = (
+        (MALE,'М'),
+        (FEMALE,'Ж')
+    )
+    user = models.OneToOneField(User,unique=True,null=False,db_index=True,on_delete=models.CASCADE)
+    about = models.TextField(verbose_name='О себе',blank=True, null=True)
+    gender = models.CharField(verbose_name='Пол', choices=GENDER_CHOICES,blank=True, max_length=2)
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender, instance, created,**kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
 
 
 
--- authapp/views.py
+++ authapp/views.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.contrib import auth, messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import FormView, UpdateView
 
-from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm, UserProfileEditForm
 from authapp.models import User
 from baskets.models import Basket
 from mainapp.mixin import BaseClassContextMixin, UserDispatchMixin
@@ -73,6 +72,13 @@ class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
     # success_message = 'OK'
     title = 'GeekShop - Профиль'
 
+    def post(self,request,*args,**kwargs):
+        form = UserProfileForm(data=request.POST,files=request.FILES,instance=request.user)
+        profile_form = UserProfileEditForm(request.POST,instance=request.user.userprofile)
+        if form.is_valid() and profile_form.is_valid():
+            form.save()
+        return redirect(self.success_url)
+
 
     def form_valid(self, form):
         messages.set_level(self.request,messages.SUCCESS)
@@ -85,7 +91,7 @@ class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
 
     def get_context_data(self, **kwargs):
         context = super(ProfileFormView, self).get_context_data(**kwargs)
-        context['baskets'] = Basket.objects.filter(user=self.request.user)
+        context['profile'] = UserProfileEditForm(instance=self.request.user.userprofile)
         return context
 
 class Logout(LogoutView):
