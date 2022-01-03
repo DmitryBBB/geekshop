a6fef4fff7be2bdc91ca7cb63113cc3c86b1a933
DmitryBBB <ark-bykov@yandex.ru>
2022-01-03 22:44:35 +0300

    add data photo and langs VK

--- authapp/pipelines.py
+++ authapp/pipelines.py
@@ -10,7 +10,7 @@ def save_user_profile(backend,user,response,*args,**kwargs):
     if backend.name != 'vk-oauth2':
         return
     api_url = urlunparse(('http','api.vk.com','method/users.get', None,
-                         urlencode(OrderedDict(fields=','.join(('bdate','sex','about')),access_token=response['access_token'],v=5.131)),None
+                         urlencode(OrderedDict(fields=','.join(('bdate','sex','about','photo_200','personal')),access_token=response['access_token'],v=5.131)),None
                          ))
     resp = requests.get(api_url)
     if resp.status_code != 200:
@@ -32,8 +32,18 @@ def save_user_profile(backend,user,response,*args,**kwargs):
     age = timezone.now().date().year - bdate.year
 
     user.age = age
-    if age < 100:
+    if age < 18:
         user.delete()
         raise AuthForbidden('social_core.backends.vk.VKOAuth2')
 
+    if data['photo_200']:
+        photo_link = data['photo_200']
+        photo_response=requests.get(photo_link)
+        path_photo = f'users_image/{user.pk}.jpg'
+        with open(f'media/{path_photo}','wb') as photo:
+            photo.write(photo_response.content)
+        user.image = path_photo
+
+    if data['personal']['langs']:
+        user.userprofile.langs = data['personal']['langs'][0] if len(data['personal']['langs'][0]) > 0 else 'EN'
     user.save()
