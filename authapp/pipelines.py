614187c9240f1d28b1a08b2a2e5e982383dedc5c
DmitryBBB <ark-bykov@yandex.ru>
2022-01-03 21:52:41 +0300

    working age exception

--- authapp/pipelines.py
+++ authapp/pipelines.py
@@ -32,7 +32,7 @@ def save_user_profile(backend,user,response,*args,**kwargs):
     age = timezone.now().date().year - bdate.year
 
     user.age = age
-    if age < 18:
+    if age < 100:
         user.delete()
         raise AuthForbidden('social_core.backends.vk.VKOAuth2')
 
